d1d648b101125d8ef5f47898bf98917ef0f5b006
TizianoAl <alfazaktiziano@gmail.com>
2020-06-22 20:39:47 -0300

    Custom Admin 2

--- config/biblioteca/admin.py
+++ config/biblioteca/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
 from .models import *
+
+
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'editorial']
+    list_display_links = ('titulo', 'editorial')
+
+class LibroInline(admin.TabularInline):
+    model = Libro
+    fields = ['titulo', 'editorial', 'paginas']
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'codigo']
+    list_display_links = ('nombre', 'codigo')
+    search_fields = ['nombre',]
+    inlines = [LibroInline]
+
+class EjemplarAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'localizacion', 'libro']
+    list_display_links = ('codigo', 'localizacion', 'libro')
+    search_fields = ['libro__titulo']
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+
+    list_display = ['codigo', 'nombre', 'telefono']
+    list_display_links = ('codigo', 'nombre',)
+
+    fieldsets = (
+        ('Datos', {
+            'fields': ('nombre',)
+        }),
+        ('Contacto', {
+            'fields': ('direccion', 'telefono')
+        }),
+    )
 # Register your models here.
 
-admin.site.register(Usuario)
-admin.site.register(Libro)
-admin.site.register(Ejemplar)
-admin.site.register(Autor)
+admin.site.register(Autor, AutorAdmin)
+admin.site.register(Libro, LibroAdmin)
+admin.site.register(Ejemplar, EjemplarAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
--- config/biblioteca/models.py
+++ config/biblioteca/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Autor(models.Model):
     nombre=models.CharField(max_length=20)
     codigo=models.AutoField(primary_key=True)
-    def str(self):
-        return '{}'.format(self.nombre)
+    def __str__(self):
+        return str(self.nombre)
 
 class Libro(models.Model):
     titulo=models.CharField(max_length=20)
@@ -15,15 +15,15 @@ class Libro(models.Model):
     paginas=models.IntegerField(max_length=20)
     codigo=models.AutoField(primary_key=True)
     autor=models.ForeignKey(Autor, on_delete=models.CASCADE)
-    def str(self):
-        return '{}'.format(self.titulo)
+    def __str__(self):
+        return str(self.titulo)
 
 class Ejemplar(models.Model):
-    localizacion=models.CharField(max_length=20)
-    codigo=models.AutoField(primary_key=True)
-    libro=models.ForeignKey(Libro, on_delete=models.CASCADE)
-    def str(self):
-        return '{}'.format(self.libro)
+    codigo = models.AutoField(primary_key = True)
+    localizacion = models.CharField(max_length = 30)
+    libro = models.ForeignKey(Libro, on_delete = models.CASCADE)
+    def __str__(self):
+        return str('Ejemplar ' + str(self.codigo) + ' de ' + str(self.libro))
 
 class Usuario(models.Model):
     nombre=models.CharField(max_length=20)
@@ -31,5 +31,5 @@ class Usuario(models.Model):
     direccion=models.CharField(max_length=20)
     codigo=models.AutoField(primary_key=True)
     ejemplares=models.ManyToManyField(Ejemplar)
-    def str(self):
-        return '{}'.format(self.nombre)
+    def __str__(self):
+        return str(self.nombre)
